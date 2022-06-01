6a7a90201494844fe4433d1ed6ee0500a05c5a47
Paulo <paulo.pertierra@gmail.com>
2022-06-01 23:25:58 +0800

    change path vue config

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dist');
+	header('Location: '.$uri.'/shop/dist');
 	exit;
 ?>
 Apache: Server error; unable to process request.
